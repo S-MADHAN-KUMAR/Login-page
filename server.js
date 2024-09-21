be1e9b940fae3bf9360b468448fca75563659c00
S-MADHAN-KUMAR <madhankumar4195@gmail.com>
2024-09-21 09:10:18 +0530

    changes added

--- server.js
+++ server.js
@@ -66,4 +66,4 @@ app.get('/logout', (req, res) => {
 });
 
 
-app.listen(5000, () => console.log("Server running on port 3000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
